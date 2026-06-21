0550c361dc91690dea4d13114c7b03d209057a90
aarushrk <166559350+aarushkik@users.noreply.github.com>
2026-06-21 11:09:45 -0700

    Shooter should be fixed because Shooter PID removed and shooter sequence refined

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/advancedcommand/ArtifactShootCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/advancedcommand/ArtifactShootCommand.java
@@ -7,13 +7,11 @@ import com.arcrobotics.ftclib.command.WaitCommand;
 
 import org.firstinspires.ftc.teamcode.commands.subsystem.BlockerStateCommand;
 import org.firstinspires.ftc.teamcode.commands.subsystem.IntakeStateCommand;
-import org.firstinspires.ftc.teamcode.commands.subsystem.ShooterStateCommand;
 import org.firstinspires.ftc.teamcode.commands.subsystem.StopStateCommand;
 import org.firstinspires.ftc.teamcode.hardware.Robot;
 import org.firstinspires.ftc.teamcode.hardware.subsystems.IntakeSubsystem;
 import org.firstinspires.ftc.teamcode.hardware.subsystems.ShooterSubsystem;
 import org.firstinspires.ftc.teamcode.util.Constants;
-import org.firstinspires.ftc.teamcode.util.Globals;
 
 public class ArtifactShootCommand extends SequentialCommandGroup {
     public
@@ -31,10 +29,19 @@ public class ArtifactShootCommand extends SequentialCommandGroup {
                 new IntakeStateCommand(IntakeSubsystem.IntakeState.IN),
                 new WaitCommand(Constants.shootDelay),
                 new StopStateCommand(ShooterSubsystem.StopState.REVERSE),
+                new IntakeStateCommand(IntakeSubsystem.IntakeState.STOP),
+                new WaitCommand(Constants.shootPauseDelay),
+                new StopStateCommand(ShooterSubsystem.StopState.READY),
+                new IntakeStateCommand(IntakeSubsystem.IntakeState.IN),
+                new WaitCommand(Constants.shootDelay),
+                new StopStateCommand(ShooterSubsystem.StopState.REVERSE),
+                new IntakeStateCommand(IntakeSubsystem.IntakeState.STOP),
+                new WaitCommand(Constants.shootPauseDelay),
+                new StopStateCommand(ShooterSubsystem.StopState.READY),
+                new IntakeStateCommand(IntakeSubsystem.IntakeState.IN),
+                new WaitCommand(Constants.shootDelay),
+                new StopStateCommand(ShooterSubsystem.StopState.REVERSE),
                 new IntakeStateCommand(IntakeSubsystem.IntakeState.STOP)
-
-
-
         );
     }
 }
